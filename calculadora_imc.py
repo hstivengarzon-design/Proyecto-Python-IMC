e51f1886443add894d31f430b7672933d553a3d3
Harold Garzon <h.stivengarzon@gmail.com>
2026-08-16 21:08:34 -0500

    Primer commit

---
+++ calculadora_imc.py
@@ -0,0 +1,45 @@
+# calculadora_imc.py
+
+'''
+Este programa almacena los datos básicos de una persona
+ (nombre, apellidos, edad, peso y estatura), para utilizarlos
+ posteriormente en el cálculo del Índice de Masa Corporal (IMC).
+'''
+
+# variable del nombre de la persona (variable tipo string)
+nombre = "Harold"
+
+# variable del apellido por parte del padre de la persona (tipo string)
+apellido_paterno = "Garzon"
+
+# variable del apellido por parte de la madre de la persona (tipo string)
+apellido_materno = "Patio"
+
+# variable de la edad de la persona (tipo int)
+edad = 18
+
+# varable del peso de la persona en kilogramos (tipo float)
+peso = 56.0
+
+# variable de la estatura de la persona en metros (tipo float)
+estatura = 1.73
+
+# verificacion de que el programa reconoce las variables
+
+# muestra "Nombre" y lo que esta almacenado en la variable nombre (tipo string)
+print("Nombre:",nombre)
+
+# muestra "Apellido paterno:" y lo que esta almacenado en la variable apellido_paterno (tipo string)
+print("Apellido paterno:",apellido_paterno)
+
+# muestra "Apellido materno:" y lo que esta almacenado en la variable apellido_materno (tipo string)
+print("Apellido materno:",apellido_materno)
+
+# muestra "Edad" y lo que esta almacenado en la variable edad (tipo int)
+print("Edad:",edad)
+
+# muestra "Peso", lo que esta almacenado en la variable edad (tipo float) y "Kg"
+print("Peso:",peso,  "kg")
+
+# muestra "Estatura", lo que esta almacenado en la variable edad (tipo float) y "m"
+print("Estatura:",estatura,  "m")
